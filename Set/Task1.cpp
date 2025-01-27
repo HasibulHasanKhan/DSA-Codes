62e8c2d4c1411bd241fcac980c769e2a21fd2182
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-01-28 00:33:31 +0600

    set method

--- Set/Task1.cpp
+++ Set/Task1.cpp
@@ -4,11 +4,38 @@ using namespace std;
 
 int main () {
     set <string> cars =  {"Volvo", "BMW", "Ford", "Mazda"};
-
+//
+ //set <int> numbers = {2,4,53,63,3};
     // loping 
     for (string car : cars) {
         cout << car << endl;
     }
+    /*
+     for (int num : numbers) {
+      cout << num << endl;
+     }
+
     
+    */
+
+   /*
+    // sort the element :
+
+    set <int , greater<int>> numbers = {2,43,5,33,3};
+   */
+
+  // add element :
+
+  cars.insert("tesla");
+
+// remove element
+ cars.erase("tesla");
+
+ // remove all element :
+
+//  cars.clear();
+
+
+
     return 0;
 }
